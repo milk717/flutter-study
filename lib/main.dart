c90e5e5e80ddf036e8b707ee576630141df4d810
milk717 <robolindasoo@gmail.com>
2022-04-26 19:23:46 +0900

    당근마켓 Expanded 사용

--- lib/main.dart
+++ lib/main.dart
@@ -10,14 +10,54 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(),
-        body: Row(
-          children: [
-            Flexible(child: Container(width: 100,color: Colors.green,), flex:3),
-            Expanded(child: Container(color: Colors.blue,)),
-            Container(width: 100,color: Colors.green,)
-          ],
-        )
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            title: Row(
+              children: const [
+                Text('대야동',style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold
+                ),),
+                Icon(Icons.keyboard_arrow_down_sharp,color: Colors.black),
+              ],
+            ),
+            actions: <Widget> [ //const 하면 안됨 왜???? 아직은 모름
+              IconButton(icon: Icon(Icons.search) ,color: Colors.black, onPressed: (){ print('click');}),
+              IconButton(icon: Icon(Icons.menu) ,color: Colors.black, onPressed: (){ print('click');}),
+              IconButton(icon: Icon(Icons.notifications_none) ,color: Colors.black, onPressed: (){ print('click');}),
+            ],
+          ),
+          body: Container(
+            width: double.infinity,
+            height: 150,
+            padding: EdgeInsets.all(8),
+            child: Row(
+              children: <Widget>[
+                Image.asset("assets/conan.png", width: 150,),
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.all(8),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text('PS5 플레이스테이션5 미개봉', style: TextStyle(fontSize: 18,),),
+                        Text('시흥시 대야동 · 끌올 17분 전',style: TextStyle(color: Colors.grey),),
+                        Text('800,000원', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: <Widget>[
+                            IconButton(onPressed: (){print('click');}, icon: Icon(Icons.favorite_border) ),
+                            Text('17'),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
       ),
     );
   }
