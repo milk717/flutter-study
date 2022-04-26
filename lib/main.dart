482fdc65fa2a54221dde9a53acdb5ca9391df355
milk717 <robolindasoo@gmail.com>
2022-04-26 10:52:11 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());  //앱 시작해주세요~
+}
+//메인페이지 만들기
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    );
+  }
+}
