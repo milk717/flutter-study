7520bce6aa08ca905e65e55bb7b494f11aab2f81
milk717 <robolindasoo@gmail.com>
2022-04-26 17:28:24 +0900

    당근마켓 상품 레이아웃 만들어보기

--- lib/main.dart
+++ lib/main.dart
@@ -11,17 +11,55 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('앱 이름'),
-          leading: Icon(Icons.add_box_outlined),
-          actions: const [Icon(Icons.star), Icon(Icons.airplanemode_active)],
-        ), //상단
+          backgroundColor: Colors.white,
+          title: Row(
+            children: const [
+              Text('대야동',style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold
+              ),),
+              Icon(Icons.keyboard_arrow_down_sharp,color: Colors.black),
+            ],
+          ),
+          actions: <Widget> [ //const 하면 안됨 왜???? 아직은 모름
+            IconButton(icon: Icon(Icons.search) ,color: Colors.black, onPressed: (){ print('click');}),
+            IconButton(icon: Icon(Icons.menu) ,color: Colors.black, onPressed: (){ print('click');}),
+            IconButton(icon: Icon(Icons.notifications_none) ,color: Colors.black, onPressed: (){ print('click');}),
+          ],
+        ),
         body: SizedBox(
-          child: IconButton(
-            icon: Icon(Icons.star),
-            onPressed: (){},
-          )
+          width: double.infinity,
+          height: 150,
+          child: Container(
+            padding: EdgeInsets.all(8),
+            child: Row(
+              children: <Widget>[
+                Image.asset("assets/conan.png", width: 150,),
+                Container(
+                  padding: EdgeInsets.all(8),
+                  width: 300,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('PS5 플레이스테이션5 미개봉', style: TextStyle(fontSize: 18,),),
+                      Text('시흥시 대야동 · 끌올 17분 전',style: TextStyle(color: Colors.grey),),
+                      Text('800,000원', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          IconButton(onPressed: (){print('click');}, icon: Icon(Icons.favorite_border) ),
+                          Text('17'),
+                        ],
+                      )
+                   ],
+                  ),
+                )
+              ],
+            )
+          ),
         )
-      )
+      ),
     );
   }
 }
