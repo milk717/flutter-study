85f561c1f1d9ee66e26f0c3dcfb59c117745bbb4
milk717 <robolindasoo@gmail.com>
2022-04-26 14:53:02 +0900

    BoxDecoration

--- lib/main.dart
+++ lib/main.dart
@@ -9,25 +9,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold( //레이아웃을 상 중 하로 나누어줌
-        appBar: AppBar(
-          title: Text('앱 이름'),
-        ), //상단
-        body: Text('안녕'),
-        bottomNavigationBar: BottomAppBar(
-          child: SizedBox(
-            height: 60,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: const [
-                Icon(Icons.phone),
-                Icon(Icons.message),
-                Icon(Icons.contact_page),
-              ],
+        home: Scaffold( //레이아웃을 상 중 하로 나누어줌
+          appBar: AppBar(
+            title: Text('앱 이름'),
+          ), //상단
+          body: Align(  //박스 위치 정렬
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              width: double.infinity,height: 150, //color: Colors.blue, -> BoxDecoration 을 키면 box 안에서만 색상지정해야함
+              margin: EdgeInsets.all(20),
+              padding: EdgeInsets.fromLTRB(50, 70, 50, 0),
+              child: Text('hello'),
+              decoration: BoxDecoration(  //세부적인 디자인할때
+                  border: Border.all(color: Colors.blueAccent)
+              ),
             ),
           ),
-        ),
-      )
+        )
     );
   }
 }
