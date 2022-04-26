1d547c40f5546ed372a62ac61c6ec66fd18ff306
milk717 <robolindasoo@gmail.com>
2022-04-26 11:22:52 +0900

    Scaffold 개념

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: Scaffold( //레이아웃을 상 중 하로 나누어줌
+        appBar: AppBar(), //상단
+        body: Center(
+          child: Container(
+            color: Colors.blueGrey,
+            height: 300,
+            child: Row(  //body == 중단, Row == 레이아웃을 가로로 배치 (Column == 세로로 배치)
+              mainAxisAlignment: MainAxisAlignment.center,  //가로축 정렬 (CSS flexBox 랑 비슷한가???)
+              crossAxisAlignment: CrossAxisAlignment.center,  //세로축 정렬 
+              /*
+                현재 Row 여서 정축이 가로니까 mainAxisAlignment 가 가로축이 되고,
+                crossAxisAlignment 가 세로축이 된다. Column 으로 설정 시 반대
+              */
+              children: const [
+                Icon(Icons.star),
+                Icon(Icons.star),
+                Icon(Icons.star),
+              ],
+            ),
+          ),
+        ),
+          bottomNavigationBar: Text("Bottom"),  //하단
+      )
     );
   }
 }
