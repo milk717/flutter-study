07eeadd991da18ee8c254dec4d4a5ae7ed28a7f7
milk717 <robolindasoo@gmail.com>
2022-05-05 13:21:42 +0900

    ListTile 목록 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      home: MyApp()
-    )
-  );  //앱 시작해주세요~
+  runApp(MaterialApp(home: MyApp())); //앱 시작해주세요~
 }
 
 //메인페이지 만들기
@@ -16,77 +13,74 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var a = 1;
-  plus(){
+  List nameList = ['코난','나루토','사스케','원피스'];
+
+  addName(String name){
     setState(() {
-      a++;
+      nameList.add(name);
     });
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        floatingActionButton: Builder(
-          builder: (context) {
-            return FloatingActionButton(
-              onPressed: (){
-                showDialog(context: context, builder: (context){
-                  return DialogUI(plus : plus);
-                });
-              },
+      floatingActionButton: Builder(builder: (context) {
+        return FloatingActionButton(
+          onPressed: () {
+            showDialog(context: context, builder: (context){
+              return DialogUI(addName: addName,);
+            });
+          },
+        );
+      }),
+      appBar: AppBar(),
+      body: ListView.builder(
+          itemCount: nameList.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: Container(
+                width: 40,
+                height: 40,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                    color: Colors.black),
+                child: Icon(
+                  Icons.person_rounded,
+                  color: Colors.white,
+                ),
+              ),
+              title: Text('${nameList[index]}'),
             );
-          }
-        ),
-        appBar: AppBar(),
-        body: Text(a.toString()),
-        bottomNavigationBar: BottomAppBar(
-          child: SizedBox(
-            height: 60,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget> [
-                IconButton(icon: Icon(Icons.phone),onPressed: (){},),
-                IconButton(icon: Icon(Icons.message),onPressed: (){},),
-                IconButton(icon: Icon(Icons.contact_page),onPressed: (){},),
-              ],
-            ),
-          ),
-        ),
-      );
-  }
-}
-
-class DialogUI extends StatelessWidget {
-  const DialogUI({Key? key, this.plus}) : super(key: key);
-  final plus;
-
-  @override
-  Widget build(BuildContext context) {
-    return Dialog(child:
-    Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [TextField(),Text(''),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            TextButton(
-              child: Text('확인'),
-              onPressed: (){
-                plus();
-                Navigator.pop(context);
-              },
-            ),
-            TextButton(
-              child: Text('취소'),
-              onPressed: (){
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        )],
-    ),
+          }),
+      bottomNavigationBar: BottomAppBar(child: Text('bottomBar'),),
     );
   }
 }
 
 
+class DialogUI extends StatelessWidget {
+  DialogUI({Key? key, this.addName}) : super(key: key);
+  final addName;
+  var inputName = TextEditingController();
 
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      title: Text('이름 추가 등록'),
+      content: TextField(
+        controller: inputName,
+      ),
+      actions: <Widget>[
+        TextButton(child: Text('추가'),onPressed: (){
+          if(inputName.text.isNotEmpty){
+            addName(inputName.text);
+          }
+          Navigator.pop(context);
+        },)
+      ],
+    );
+  }
+}
