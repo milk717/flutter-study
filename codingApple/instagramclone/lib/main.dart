908c128f4881dfed41bbe0d4deed25458e7cdc14
milk717 <robolindasoo@gmail.com>
2022-05-28 23:35:55 +0900

    instagram clone: post layout

--- codingApple/instagramclone/lib/main.dart
+++ codingApple/instagramclone/lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String result = '';
+  int index = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -30,10 +31,17 @@ class _MyAppState extends State<MyApp> {
           )
         ],
       ),
-      body: Center(child: Text(result),),
+      body: (index==0)?HomeWidget():ShoppingWidget(result: result,),
       bottomNavigationBar: BottomNavigationBar(
         showSelectedLabels: false,
         showUnselectedLabels: false,
+        currentIndex: index,
+        onTap: (i){
+          print(i);
+          setState((){
+            index = i;
+          });
+        },
         items: <BottomNavigationBarItem> [
           BottomNavigationBarItem(
             icon: Icon(Icons.home_outlined),
@@ -58,5 +66,40 @@ class _MyAppState extends State<MyApp> {
         child: const Icon(Icons.file_download),
       ),
     );
+  }
+}
+
+class HomeWidget extends StatelessWidget {
+  const HomeWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: 3,
+        itemBuilder: (context,index){
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset('assets/conan.jpg'),
+              Text('좋아요 100'),
+              Text('글쓴이'),
+              Text('글내용'),
+            ],
+          );
+        }
+    );
+  }
+}
+
+class ShoppingWidget extends StatelessWidget {
+  ShoppingWidget({
+    Key? key,
+    this.result
+  }) : super(key: key);
+
+  String? result;
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('$result'));
   }
 }
